5265bbd407305ea3fca6cc6680b9bcc6b0b8399d
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2025-02-18 20:49:00 +0200

    slight changes/fix to the global.css file and layout with TW classes used causing a conflict  with the DB layout.tsx file.

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,80 @@
-import Image from "next/image";
+//import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  BrainCogIcon,
+  EyeIcon,
+  GlobeIcon,
+  MonitorSmartphoneIcon,
+  ServerCogIcon,
+  ZapIcon,
+} from "lucide-react";
+import Link from "next/link";
 
+const features =[
+  {
+    name: "Store your PDF Documents",
+    description:"Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon:GlobeIcon,
+  },
+
+  {
+    name: "Blazing Fast Responses",
+    description:"Expeirience lightning fast responses from our AI powered chatbot, enuring you get the answers you need in seconds.",
+    icon: ZapIcon,
+  },
+
+  {
+    name: "Chat Memorisation",
+    description:"Our inteligent chatbot remembers your previous conversations, ensuring you get the most relevant answers.",
+    icon:BrainCogIcon,
+  },
+
+  {
+    name: "interactive PDF Viewer",
+    description:"Engage with your PDFs like never before using our intuitive PDF viewer.",
+    icon:EyeIcon,
+  },
+  {
+    name: "Cloud backup",
+    description:"Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon:ServerCogIcon,
+  },
+
+  {
+    name: "Responsive Across Devices",
+    description:"Keep all your important PDF files securely stored and easily accessible anytime, anywhere.",
+    icon:MonitorSmartphoneIcon,
+  }
+]
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+   
+      <main className="overflow-scroll bg-gradient-to-bl p-2 lg:p-5 from-white to-indigo-600 flex-1">
+        <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
+         <div className="flex flex-col justify-center items-center mx-auto max-w-7xl px-6 lg:px-8">
+           <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-base font-semibold leading-7 text-indigo-600">Your Interactive Document Companion</h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              Transform Your PDFs into Interactive Conversations 
+            </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Introducing{" "}
+              <span className="font-bold text-indigo-600">Chat with PDF.</span>
+             <br />
+             <br /> Upload your document, and our chatbot will answer your questions, sumumarize content, and more. <span className="font-bold text-indigo-600">
+              Chat with PDF</span>{" "}
+              <span className="font-bold text-indigo-600">dynamic conversations</span>,
+              enhancing productivity 10x fold effortlessly.
+            </p>
+
+            <Button asChild className="mt-10">
+              <Link href="/dashboard">Get Started</Link>
+            </Button>
+           </div>
+         </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+  ); 
+} 
