e3a83e8c6c0ea2b94a502bafa9784a73ab34a6ee
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2025-03-03 15:04:42 +0200

    feat: implement file upload functionality with FileUploader component and add upload page refactor: remove unused UserProfile component and clean up DashboardLayout

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,10 @@
+import FileUploader from '../../../components/FileUploader'
+function Uploadpage() {
+  return (
+    <div>
+        <FileUploader />
+    </div>
+  )
+}
+
+export default Uploadpage
