ec2a8ab11c72f23c94c6a597fb968d7b24962367
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2025-03-03 13:15:45 +0200

    style: update PlaceholderDocument button styling for improved layout and appearance

--- components/PlaceholderDocument.tsx
+++ components/PlaceholderDocument.tsx
@@ -2,10 +2,10 @@ import { Button } from '@/components/ui/button';
 import { PlusCircleIcon } from 'lucide-react';
 function PlaceholderDocument() {
   return (
- <Button>
+ <Button className="flex flex-col items-center justify-center w-64
+ h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400">
      <PlusCircleIcon className="h-16 w-16 " />
     <p>Add a document</p>
  </Button>  )
 }
-
 export default PlaceholderDocument
