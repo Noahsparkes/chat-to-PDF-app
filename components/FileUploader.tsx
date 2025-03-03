062d8f02078546876020013af7b7a64ff5ccd631
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2025-03-03 16:04:36 +0200

    feat: enhance FileUploader component with improved drag-and-drop UI and visual feedback

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,30 +1,46 @@
 'use client';
-import {useCallback} from 'react';
-import {useDropzone} from 'react-dropzone';
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import {CheckCircleIcon,
+       CircleArrowDown,
+       HammerIcon,
+       RocketIcon,
+       SaveIcon
+} from 'lucide-react';
+
 function FileUploader() {
-    const onDrop = useCallback((acceptedFiles: File[]) => {
-        // Do something with the files
-    }, []);
-    const {getRootProps, getInputProps, isDragActive, isFocused} = useDropzone({onDrop});
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, isFocused, isDragAccept } = useDropzone({ onDrop });
 
   return (
-<div className="fex flex-col gap-4 items-center max-w-7xl mx-auto text-center">
-    {/* Loading state */}
-    <div {...getRootProps()}
-    className={`p-10 border-2 border-dashed mt-10 w-[90%] border-indigo-600 text-indigo-600
-         rounded-lg h-96 flex items-center `}
-    >
-        
-      <input {...getInputProps()} />
-      {
-      
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      } 
+    <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto ">
+      {/* Loading state */}
+      <div {...getRootProps()} className={`p-10 border-2 border-dashed mt-10 w-[90%]
+         border-indigo-600 text-indigo-600 rounded-lg
+          h-96 flex items-center justify-center ${isFocused || isDragAccept ? 'bg-indigo-400' : 'bg-indigo-100'}`}>
+        <input {...getInputProps()} />
+
+
+        <div className="flex flex-col items-center justify-center ">
+          {isDragActive ? (
+            <>
+            <RocketIcon className="h-20 w-20 animate-ping" />
+            <p>Drop the files here ...</p>
+            </>
+          ) : (
+            <>
+            <CircleArrowDown className="h-20 w-20 animate-bounce" />
+             <p>Drag &apos;n&apos; drop some files here, or click to select files</p>
+            </>
+            
+          )}
+        </div>
+      </div>
     </div>
-</div>
-  )
+  );
 }
 
-export default FileUploader
+export default FileUploader;
