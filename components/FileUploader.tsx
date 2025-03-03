06b3cb384fc54885baf889967c24c2503fa78b44
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2025-03-03 15:32:46 +0200

    feat: enhance FileUploader component with drag-and-drop functionality

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,7 +1,29 @@
 'use client';
+import {useCallback} from 'react';
+import {useDropzone} from 'react-dropzone';
 function FileUploader() {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        // Do something with the files
+    }, []);
+    const {getRootProps, getInputProps, isDragActive, isFocused} = useDropzone({onDrop});
+
   return (
-    <div>FileUploader</div>
+<div className="fex flex-col gap-4 items-center max-w-7xl mx-auto text-center">
+    {/* Loading state */}
+    <div {...getRootProps()}
+    className={`p-10 border-2 border-dashed mt-10 w-[90%] border-indigo-600 text-indigo-600
+         rounded-lg h-96 flex items-center `}
+    >
+        
+      <input {...getInputProps()} />
+      {
+      
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      } 
+    </div>
+</div>
   )
 }
 
